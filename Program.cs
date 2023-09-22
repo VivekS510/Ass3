abe0f74f9da5592283f9b2fced98122c52659fea
Vivek <svivek1507@gmail.com>
2023-09-21 23:29:52 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Value: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+            if (value % 3 == 0 || value % 7 == 0)
+            {
+                Console.WriteLine("True");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("False");
+                Console.ReadLine();
+            }
+        }
+    }
+}
